29ece4ff30d423149d219e9e3bec39882fc6f29e
Marcel Savegnago <marcel.savegnago@gmail.com>
2025-11-10 20:52:15 -0300

    [HACK] sale_project: possible problem with l10n_br_account and _fiscal

--- addons/sale_project/models/project.py
+++ addons/sale_project/models/project.py
@@ -105,7 +105,7 @@ class Project(models.Model):
         query.order = None
         query_string, query_param = query.select(
             'jsonb_object_keys(account_move_line.analytic_distribution) as account_id',
-            'COUNT(DISTINCT move_id) as move_count',
+            'COUNT(DISTINCT account_move_line.move_id) as move_count',
         )
         query_string = f"{query_string} GROUP BY jsonb_object_keys(account_move_line.analytic_distribution)"
         self._cr.execute(query_string, query_param)
@@ -186,7 +186,7 @@ class Project(models.Model):
         query = self.env['account.move.line']._search([('move_id.move_type', 'in', ['out_invoice', 'out_refund'])])
         query.add_where('account_move_line.analytic_distribution ? %s', [str(self.analytic_account_id.id)])
         query.order = None
-        query_string, query_param = query.select('DISTINCT move_id')
+        query_string, query_param = query.select('DISTINCT account_move_line.move_id')
         self._cr.execute(query_string, query_param)
         invoice_ids = [line.get('move_id') for line in self._cr.dictfetchall()]
         action = {
@@ -596,7 +596,7 @@ class Project(models.Model):
         query = self.env['account.move.line']._search([('move_id.move_type', 'in', ['in_invoice', 'in_refund'])])
         query.add_where('account_move_line.analytic_distribution ? %s', [str(self.analytic_account_id.id)])
         query.order = None
-        query_string, query_param = query.select('DISTINCT move_id')
+        query_string, query_param = query.select('DISTINCT account_move_line.move_id')
         self._cr.execute(query_string, query_param)
         vendor_bill_ids = [line.get('move_id') for line in self._cr.dictfetchall()]
         action_window = {
